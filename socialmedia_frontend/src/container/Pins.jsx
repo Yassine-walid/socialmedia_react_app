b4b241a932172d55f828dc27048c7312af59f400
Yassine walid <yassine1998walid@gmail.com>
2022-11-12 20:49:01 +0100

    First Commit

--- socialmedia_frontend/src/container/Pins.jsx
+++ socialmedia_frontend/src/container/Pins.jsx
@@ -1,9 +1,26 @@
 import React, {useState} from 'react'
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 
-const Pins = () => {
+import { Navbar, Feed, PinDetail, CreatePin, Search  } from "../components/index";
+
+const Pins = ({ user }) => {
+
+  const [serchTerm, setSerchTerm] = useState('')
   return (
-    <div>Pins</div>
+    <div className="px-2 md:px-5">
+      <div className="bg-gray-50">
+        <Navbar serchTerm={serchTerm} setSerchTerm={setSerchTerm}  user={user}/>
+      </div>
+      <div className="h-full">
+        <Routes>
+          <Route path="/" element={<Feed />} />
+          <Route path="/category/:categoryId" element={<Feed />} />
+          <Route path="/pin-detail/:pinId" element={<PinDetail user={user} />} />
+          <Route path="/create-pin" element={<CreatePin user={user} />} />
+          <Route path="/search" element={<Search serchTerm={serchTerm} setSerchTerm={setSerchTerm}  />} />
+        </Routes>
+      </div>
+    </div>
   )
 }
 
